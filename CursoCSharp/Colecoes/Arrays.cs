cb99cd4065e6e12830a8e3a959444e2d8abfcf9d
Emily Sena <emily.o.sena@hotmail.com>
2020-10-08 18:14:19 -0300

    "ok"

--- CursoCSharp/Colecoes/Arrays.cs
+++ CursoCSharp/Colecoes/Arrays.cs
@@ -5,8 +5,26 @@ using System.Text;
 namespace CursoCSharp.Colecoes{
     class Array{
         public static void Executar() {
-            string[] alunos = new string[5];
-            
+            string[] aluno = new string[4];
+            aluno[0] = "Manuela";
+            aluno[1] = "Bruno";
+            aluno[2] = "Rafael";
+            aluno[3] = "Gabi";
+            foreach (var pessoa in aluno){
+                Console.WriteLine(pessoa);
+            }
+            double soma = 0;
+            double[] notas = {9.3, 5.7, 8.7, 6.5};
+            foreach (var nota in notas){
+                soma += nota;
+            }
+            double media = soma/notas.Length;
+            Console.WriteLine(media);
+
+            char[] letras =  {'C', 'a', 'l', 'o', 'r'};
+            string palavra = new string(letras);
+            Console.WriteLine(palavra);
+
         }
     }
 
---
+++ CursoCSharp/Colecoes/List.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos{
+
+    class ColecoesList{
+        public static void Executar() {
+            
+        }
+    }
+
+
+
+}
